5dda4a5f158f9370b1c0cad0da8c99491019a92c
carlogarro <carlogarro@gmail.com>
2024-09-24 18:16:19 +0200

    added port for web

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
